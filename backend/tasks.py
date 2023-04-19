12eb36f35bf91b0a5523ad2805fa8783cd914ec4
Mehdi Rtal <mehdirtal7@pm.me>
2023-04-19 00:32:02 +0000

     

--- backend/tasks.py
+++ backend/tasks.py
@@ -3,11 +3,7 @@ import time
 from worker import app
 
 
-# @app.on_after_configure.connect
-# def setup_periodic_tasks(sender, **kwargs):
-#     sender.add_periodic_task(10, check_emails, name='add every 10')
-
-@app.task(ignore_result=True)
-def check_emails(task_type):
-    time.sleep(int(task_type) * 10)
-    return True
+@app.task
+def test():
+    time.sleep(5)
+    return 'test'
