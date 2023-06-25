644350d30ec8fdaa869b3e6eaf5d5b9c058ead25
Mehdi Rtal <70097433+MehdiRtal@users.noreply.github.com>
2023-06-25 21:13:10 +0000

    Update nuxt.config.ts

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -5,19 +5,9 @@ export default defineNuxtConfig({
         "@nuxtjs/turnstile",
         "nuxt-api-party",
         "nuxt-gtag",
-        "nuxt-quasar-ui",
         "@unocss/nuxt",
         "@vueuse/nuxt",
     ],
-    quasar: {
-        config: {
-            ripple: false,
-        },
-        extras: {
-            fontIcons: ["material-icons"],
-        },
-        iconSet: "material-icons",
-    },
     turnstile: {
         siteKey: process.env.TURNSTILE_SITE_KEY,
         secretKey: process.env.TURNSTILE_SECRET_KEY,
