d2c6129133e4d0d7f180e20859394a63c9a3f044
MehdiRtal <mehdirtal7@pm.me>
2024-01-19 15:42:02 +0100

    Update Redis client instantiation

--- api/redis_.py
+++ api/redis_.py
@@ -1,14 +1,14 @@
 from fastapi import Depends
-from redis.asyncio.client import Redis as Redis_
+from redis.asyncio.client import Redis
 from typing import Annotated
 
 from config import settings
 
 
-connection = Redis_.from_url(str(settings.REDIS_URL))
+connection = Redis.from_url(str(settings.REDIS_URL))
 
 async def get_redis_session():
     async with connection.client() as session:
         yield session
 
-RedisSession = Annotated[Redis_, Depends(get_redis_session)]
+RedisSession = Annotated[Redis, Depends(get_redis_session)]
