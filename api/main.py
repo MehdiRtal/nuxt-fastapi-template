146e397d91c3d7b6a69430d39756c8c4c9335214
MehdiRtal <mehdirtal7@pm.me>
2024-01-24 11:25:44 +0100

    Refactor API initialization and add Prometheus instrumentation

--- api/main.py
+++ api/main.py
@@ -2,11 +2,8 @@ from fastapi import FastAPI, Request, Depends, status
 from fastapi.exceptions import RequestValidationError
 from starlette.exceptions import HTTPException
 from fastapi.middleware.gzip import GZipMiddleware
-from starlette.middleware.errors import ServerErrorMiddleware
 from fastapi_limiter.depends import RateLimiter
 from contextlib import asynccontextmanager
-from prometheus_fastapi_instrumentator import Instrumentator
-
 
 from api.sentry import init_sentry
 from api.prometheus import init_prometheus
@@ -21,28 +18,29 @@ import api.users
 import api.items
 
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_db()
+    init_cache()
+    await init_limiter()
+    if settings.ENVIRONEMENT.is_prod:
+        init_sentry()
+    yield
+
 app = FastAPI(
+    debug=True if settings.ENVIRONEMENT.is_dev else False,
     title="API",
+    lifespan=lifespan,
     default_response_class=DefaultORJSONResponse,
     dependencies=[Depends(valid_signature), Depends(RateLimiter(times=100, minutes=1))],
     docs_url="/docs" if settings.ENVIRONEMENT.is_dev else None,
     redoc_url="/redoc" if settings.ENVIRONEMENT.is_dev else None
 )
 
-@app.on_event("startup")
-async def startup():
-    await init_db()
-    init_cache()
-    await init_limiter()
-    if settings.ENVIRONEMENT.is_prod:
-        init_sentry()
-    Instrumentator().instrument(app, metric_namespace='myproject', metric_subsystem='myservice').expose(app)
-    # init_prometheus(app)
+if settings.ENVIRONEMENT.is_dev:
+    init_prometheus(app)
 
 app.add_middleware(GZipMiddleware)
-
-if settings.ENVIRONEMENT.is_dev:
-    app.add_middleware(ServerErrorMiddleware, debug=True)
 
 @app.exception_handler(HTTPException)
 def http_exception_handler(request: Request, exception: HTTPException):
--- api/prometheus.py
+++ api/prometheus.py
@@ -1,5 +1,8 @@
-from prometheus_fastapi_instrumentator import Instrumentator
+from fastapi import FastAPI
+from prometheus_fastapi_instrumentator.instrumentation import PrometheusFastApiInstrumentator
 
 
-def init_prometheus(app):
-    Instrumentator().instrument(app).expose(app)
+prometheus = PrometheusFastApiInstrumentator()
+
+def init_prometheus(app: FastAPI):
+    prometheus.instrument(app).expose(app)
