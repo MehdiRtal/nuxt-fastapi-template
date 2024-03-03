f45e2164627351bfba9eab4fb1631d8e82e0eb67
MehdiRtal <mehdirtal7@pm.me>
2024-03-03 10:58:44 +0100

     

--- api/src/utils.py
+++ api/src/utils.py
@@ -3,4 +3,7 @@ from fastapi.responses import ORJSONResponse
 
 class DefaultORJSONResponse(ORJSONResponse):
     def render(self, content):
-        return super().render({"status": "success", "data": content})
+        if isinstance(content, dict):
+            return super().render({"status": "success", **content})
+        else:
+            return super().render({"status": "success", "data": content})
